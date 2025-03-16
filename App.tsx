578c415be27b0141bf852bfa93ce798a18db4b9d
Sujalakasl <sujallamichhaneakasl@gmail.com>
2025-03-16 21:36:51 +0545

    Apploaging fixed

---
+++ App.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import  AppLoading  from "expo-app-loading";
+
+import navigationTheme from "./app/navigation/navigationTheme";
+import AppNavigator from "./app/navigation/AppNavigator";
+import OfflineNotice from "./app/components/OfflineNotice";
+import AuthNavigator from "./app/navigation/AuthNavigator";
+import AuthContext from "./app/auth/context";
+import authStorage from "./app/auth/storage";
+import { navigationRef } from "./app/navigation/rootNavigation";
+import logger from "./app/utility/logger";
+
+logger.start();
+
+export default function App() {
+  const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
+
+  const restoreUser = async () => {
+    const user = await authStorage.getUser();
+    if (user) setUser(user);
+  };
+
+  const handleLoadingError = () => {
+    throw new Error('App loading error');
+  };
+  
+  if (!isReady)
+    return (
+      <AppLoading startAsync={restoreUser} onError= {handleLoadingError} onFinish={() => setIsReady(true)} />
+    );
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      <OfflineNotice />
+      <NavigationContainer ref={navigationRef} theme={navigationTheme}>
+        {user ? <AppNavigator /> : <AuthNavigator />}
+      </NavigationContainer>
+    </AuthContext.Provider>
+  );
+}
